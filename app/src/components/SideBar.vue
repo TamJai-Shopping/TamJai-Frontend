ec664eed93cc9fa2a999020e9c306d3128bf9fbd
KenzieLeonic <57589336+KenzieLeonic@users.noreply.github.com>
2022-10-09 12:10:09 +0700

    add slidebar in market

--- app/src/components/SideBar.vue
+++ app/src/components/SideBar.vue
@@ -7,7 +7,7 @@
         <path d="M1.5 1.5A.5.5 0 0 1 2 1h12a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.128.334L10 8.692V13.5a.5.5 0 0 1-.342.474l-3 1A.5.5 0 0 1 6 14.5V8.692L1.628 3.834A.5.5 0 0 1 1.5 3.5v-2zm1 .5v1.308l4.372 4.858A.5.5 0 0 1 7 8.5v5.306l2-.666V8.5a.5.5 0 0 1 .128-.334L13.5 3.308V2h-11z"/>
       </div>
       <div>
-      <select v-model="selected" class="px-14 rounded-lg bg-gray-100 border-gray-300 my-3" >
+      <select v-model="selected" class="w-36 rounded-lg bg-gray-100 border-gray-300 my-3" >
         <option disabled value="">เลือกหมวดหมู่</option>
         <option value="profile">โปรไฟล์</option>
         <option value="setting">การตั้งค่า</option>
@@ -17,9 +17,9 @@
       <div>
         <h2 class="my-3">เลือกตามช่วงราคา</h2>
         <div class="flex">
-        <input type="text" class="border border-2 rounded-xl px-4 py-2 w-24 h-8 mr-1.5" name="minPrice" placeholder="ราคาต่ำที่สุด"/>
+        <input type="text" class="border border-2 text-xs rounded-xl px-4 py-2 w-24 h-8 mr-1.5" name="minPrice" placeholder="ราคาต่ำที่สุด"/>
         <p> - </p>
-        <input type="text" class="border border-2 rounded-xl px-4 py-2 w-24 h-8 ml-1.5" name="maxPrice" placeholder="ราคาสูงสุด"/>
+        <input type="text" class="border border-2 text-xs rounded-xl px-4 py-2 w-24 h-8 ml-1.5" name="maxPrice" placeholder="ราคาสูงสุด"/>
         <button class="rounded-lg "></button>
         </div>
       </div>
