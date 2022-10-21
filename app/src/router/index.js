ff1e284588ca7eb4d052c9e2a5b53c6a8b630fa9
aungpor <napat.patc@ku.th>
2022-10-21 12:28:57 +0700

    UserDataView

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -10,32 +10,9 @@ const router = createRouter({
       component: Marketplace
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    },
-    {
-      path: '/coin-desk',
-      name: 'coin-desk',
-      component: CoinDeskView
-    },
-    {
-      path: '/reward',
-      name: 'rewards',
-      component: () => import('../views/reward/AllRewardView.vue')
-    },
-    {
-      path: '/reward/:id',
-      name: 'reward.show',
-      component: () => import('../views/reward/ShowView.vue')
-    },
-    {
-      path: '/reward/create',
-      name: 'reward.create',
-      component: () => import('../views/reward/CreateView.vue')
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/user/UserDataView.vue')
     }
   ]
 })
---
+++ app/src/views/user/UserDataView.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <h1 class="text-3xl text-yellow-700">Aungpor Userdata</h1>
+    </div>
+    <div>
+        <div class="mx-5 bg-gray-400">
+            
+            <div class="bg-white">
+                <h1>แก้ไขข้อมูลผู้ใช้</h1>    
+                <img src="https://pbs.twimg.com/media/FXSOQxdVUAELv2U?format=png&name=small" class="m-1 p-3" width="150" height="150">
+                <a href="#" class="m-2 block p-6 max-w-sm bg-[#F8F8F8] rounded-lg border border-gray-200 shadow-md hover:bg-gray-100">
+                    แก้ไขข้อมูลรูปภาพ
+                </a>
+            </div>
+            
+            <div class="bg-white">
+                <div class="">
+                    <h1>ชื่อผู้ใช้</h1>
+                    <input class="pl-2 outline-none border-none bg-gray-400" type="text"/>
+                </div>
+                <div class="">
+                    <h1>อีเมล</h1>
+                    <input class="pl-2 outline-none border-none bg-gray-400" type="text"/>
+                </div>
+                <div class="">
+                    <h1>เบอร์โทรศัพท์</h1>
+                    <input class="pl-2 outline-none border-none bg-gray-400" type="text"/>
+                </div>
+                <div class="">
+                    <h1>รหัสผ่าน</h1>
+                    <input class="pl-2 outline-none border-none bg-gray-400 " type="text"/>
+                </div>
+                <div class="">
+                    <h1>ยืนยันรหัสผ่าน</h1>
+                    <input class="pl-2 outline-none border-none bg-gray-400" type="text"/>
+                </div>
+
+            </div>
+            <div>
+                <button type="submit" class="bg-gray-400 block w-full bg-angelBaby-300 mt-4 py-2 rounded-xl text-white font-semibold mb-2">
+                    SUBMIT
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
