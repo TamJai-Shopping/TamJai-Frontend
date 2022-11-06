367a029a40aa805b236fa8945f2c697ee41bf99c
Irisia <pochara.su@ku.th>
2022-11-06 16:04:17 +0700

    count bar working

--- app/src/views/products/ProductShowView.vue
+++ app/src/views/products/ProductShowView.vue
@@ -56,8 +56,8 @@
     
     <div class="grid grid-cols-3 gap-2 px-8">
       <div class="flex justify-center items-center">
-          <div class="">
-              <h1 class="text-6xl font-bold text-gray-500">4.5</h1>
+          <div class="w-2/12">
+              <h1 class="text-6xl font-bold text-gray-500 flex justify-center">{{ product.rating }}</h1>
               <p class="flex justify-center text-sm font-medium text-gray-500 dark:text-gray-400">out of 5</p>
           </div>
           <div class="justify-center ml-4 w-full grid-rows-5 grid-cols-1 grid">
@@ -71,7 +71,7 @@
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   </div>
                   <div class="col-span-2 w-full h-2 bg-gray-100 rounded dark:bg-gray-700">
-                      <div class="h-2 bg-gray-300 rounded" style="width: 70%"></div>
+                      <div class="h-2 bg-gray-300 rounded" :style="'width: ' + product.review_stat.five * 100 / product.review_count + '%'"></div>
                   </div>
               </div>
             
@@ -83,7 +83,7 @@
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   </div>
                   <div class="col-span-2 w-full h-2 bg-gray-100 rounded dark:bg-gray-700">
-                      <div class="h-2 bg-gray-300 rounded" style="width: 17%"></div>
+                      <div class="h-2 bg-gray-300 rounded" :style="'width: ' + product.review_stat.four * 100 / product.review_count + '%'"></div>
                   </div>
               </div>
 
@@ -94,7 +94,7 @@
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   </div>
                   <div class="col-span-2 w-full h-2 bg-gray-100 rounded dark:bg-gray-700">
-                      <div class="h-2 bg-gray-300 rounded" style="width: 8%"></div>
+                      <div class="h-2 bg-gray-300 rounded" :style="'width: ' + product.review_stat.three * 100 / product.review_count + '%'"></div>
                   </div>
               </div>
 
@@ -104,7 +104,7 @@
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   </div>
                   <div class="col-span-2 w-full h-2 bg-gray-100 rounded dark:bg-gray-700">
-                      <div class="h-2 bg-gray-300 rounded" style="width: 4%"></div>
+                      <div class="h-2 bg-gray-300 rounded" :style="'width: ' + product.review_stat.two * 100 / product.review_count + '%'"></div>
                   </div>
               </div>
 
@@ -113,7 +113,7 @@
                       <svg aria-hidden="true" class="w-5 h-5 text-gray-300 dark:text-gray-500" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><title>Fifth star</title><path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path></svg>
                   </div>
                   <div class="col-span-2 w-full h-2 bg-gray-100 rounded dark:bg-gray-700">
-                      <div class="h-2 bg-gray-300 rounded" style="width: 1%"></div>
+                      <div class="h-2 bg-gray-300 rounded" :style="'width: ' + product.review_stat.one * 100 / product.review_count + '%'"></div>
                   </div>
               </div>
               
