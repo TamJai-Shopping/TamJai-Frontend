3f228441e3374a78a13782bff8efccd744f7a1ac
Ploy Putita <putita.j@ku.th>
2022-11-07 02:34:24 +0700

    add price filter

--- app/src/views/products/Marketplace.vue
+++ app/src/views/products/Marketplace.vue
@@ -1,7 +1,7 @@
 <template >
   <div class="font-mono mx-auto max-w-7xl text-gray-700">
-    <h1 class="pl-8 pt-6 text-xl">หมวดหมู่สินค้า</h1>
-    <div class="p-7 pt-4 pl-8 flex flex-wrap justify-between">
+    <h1 class="pl-8 pt-6 text-xl ml-14">หมวดหมู่สินค้า</h1>
+    <div class="font-mono p-6 pt-4 flex flex-wrap justify-start pl-20">
       <categoryCard v-for="category in categories"
                     :category="category"
                     :key="category.id"
@@ -10,7 +10,24 @@
     </div>
     <div class="pl-8 flex">
       <h1 class="pt-1.5 text-xl pl-14">สินค้าทั้งหมด</h1>
-      <div class="ml-auto pr-9">
+    </div>
+    <div class="pl-8 flex my-4">
+      <div class="flex items-center justify-center">
+           <div>
+              <label class="mr-4 ml-14">เรียงตามช่วงราคา</label>
+              <input type="number" min="0" v-model="priceRange.start" id="start_price" class="rounded-lg shadow w-32 bg-gray-100 border-gray-200 py-2">
+           </div>
+           <p class="mx-4">-</p>
+           <div>
+               <input type="number" min="0" v-model="priceRange.end" id="end_price" class="rounded-lg shadow w-32 bg-gray-100 border-gray-200 py-2">
+           </div>
+           <div class="mx-2 flex inset-y-0 left-0 items-center pl-3 cursor-pointer">
+               <button @click="priceFilter()" class="p-2.5 bg-[#528D58] border shadow rounded-lg">
+                   <svg aria-hidden="true" class="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"></path></svg>
+               </button>
+           </div>
+       </div>
+      <div class="ml-auto pr-20 mr-2">
         <label class="px-3 text-lg">เรียงโดย</label>
         <select v-model="sortOption" class="rounded-lg shadow bg-gray-100 border-gray-200 py-2">
           <option value="default">ล่าสุด</option>
@@ -58,9 +75,30 @@
             products: null,
             error: null,
             sortOption: 'default',
-        }
-    },
-    watch: {
+            priceRange: {
+                 start: null,
+                 end: null
+             }
+         }
+     }, 
+     methods: {
+         async priceFilter() {
+             if (this.priceRange.start == null && this.priceRange.end == null) {
+                 return null
+             } else if (this.priceRange.start == 0 && this.priceRange.end == 0) {
+                 return null
+             } else if (this.priceRange.end < this.priceRange.start) {
+                 return null
+             }
+             await this.product_store.fetch()
+             this.products = this.product_store.sortByLatest
+             this.products = this.products.filter(product => {
+                 return product.price >= this.priceRange.start && product.price <= this.priceRange.end
+             })
+             // TODO: ถ้า filter ได้แล้ว จะทำให้ sortOption กลับไปเป็น default
+         },
+     },
+     watch: {
      sortOption (newOption, oldOption) {
        switch (newOption) {
          case 'minPrice':
